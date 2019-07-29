87a7ea81a1b6223eace3218fa22c3832f9550075
TerenceG <1035260902@qq.com>
2019-07-29 17:40:55 +0800

    add sol

---
+++ src/com/tce/leetcodeSolution/sol92.java
@@ -0,0 +1,42 @@
+package com.tce.leetcodeSolution;
+
+public class sol92 {
+	public ListNode reverseBetween(ListNode head, int m, int n) {
+		ListNode cur = null;
+		ListNode slow = head;
+		ListNode fast = head;
+		for (int i = 1; i < m; i++) {
+			cur = slow;
+			slow = slow.next;
+		}
+		for (int j = 1; j < n; j++) {
+			fast = fast.next;
+		}
+
+		if (slow == fast)
+			return head;
+		if (cur == null)
+			head = fast;
+		else
+			cur.next = fast;
+
+		cur = slow.next;
+		slow.next = fast.next;
+
+		if (cur == fast)
+			fast.next = slow;
+		else {
+			ListNode o = cur.next;
+			while (o != fast) {
+				cur.next = slow;
+				slow = cur;
+				cur = o;
+				o = o.next;
+			}
+			cur.next = slow;
+			fast.next = cur;
+		}
+
+		return head;
+	}
+}
